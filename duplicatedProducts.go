e970cf38e7d91cb5eb93facb900fcce096f5894e
bagusbpg <bagusbpg@gmail.com>
2021-12-24 11:40:31 +0700

    minor comment revision 1

--- duplicatedProducts.go
+++ duplicatedProducts.go
@@ -13,11 +13,11 @@ weight = [2, 5, 1, 1, 1]
 A complete item description for item 0: (name[0], prices[0], weight[0] is (ball, 2, 2))
 
 	Name	Price	Weight
-	ball	2		2
-	bat	3		5
-	glove	1		1
-	glove	2		1
-	glove	1		1
+	ball	2	2
+	bat	3	5
+	glove	1	1
+	glove	2	1
+	glove	1	1
 
 The first two items are unique.
 The two gloves at indices 2 and 4 are equal in all three attributes, so there is
